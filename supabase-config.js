298dff932dc78ea9a6764478c5c19c22eca5e82f
Chandru <chandru.d171989@gmail.com>
2026-08-18 18:33:01 +0530

    new features added

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = 'https://ehecjvminmftnsjbwsoe.supabase.co';
+
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_mAIy2PkaQ6gxhKWGapW-lw_lFgRHPkO';
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
